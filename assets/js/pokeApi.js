337f35f060e67c00c300595cbd26c8dfc976d794
Álvaro Silva Garcia <alvaro_s.g@hotmail.com>
2023-12-14 17:23:05 -0300

    Adicionado métodos para a manipulação da API

--- assets/js/pokeApi.js
+++ assets/js/pokeApi.js
@@ -5,11 +5,24 @@ const pokeApi = {
 
       const req = await fetch(url);
       const bodyJson = await req.json();
-      const pokemons = await bodyJson.results;
+      const pokemons = bodyJson.results;
+      const pokemonsDetails = await this.getPokemonsDetail(pokemons);
 
-      return pokemons;
+      return pokemonsDetails;
     } catch (error) {
       console.log("Error" + error);
     }
   },
+
+  getPokemonsDetail: async function(pokemons) {
+    const pokemonsDetails = await Promise.all(
+      pokemons.map(this.fetchPokemonDetail)
+    );
+    return pokemonsDetails;
+  },
+
+  fetchPokemonDetail: async function(pokemon) {
+    const pokemonReq = await fetch(pokemon.url);
+    return pokemonReq.json();
+  }
 };
