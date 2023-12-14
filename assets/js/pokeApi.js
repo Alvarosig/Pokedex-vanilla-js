88105fe750b14f96b41c4b66eb5d45ab7dfe8442
Álvaro Silva Garcia <alvaro_s.g@hotmail.com>
2023-12-14 18:27:07 -0300

    Refatorado métodos para melhor legibidade

--- assets/js/pokeApi.js
+++ assets/js/pokeApi.js
@@ -1,12 +1,8 @@
 const pokeApi = {
-  getPokemonsApi: async function (offset = 0, limit = 10) {
+  getPokemonsApi: async function (offset = 0, limit = 8) {
     try {
-      const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
-      const req = await fetch(url);
-      const bodyJson = await req.json();
-      const pokemons = bodyJson.results;
-      const pokemonsDetails = await this.getPokemonsDetail(pokemons);
+      const pokemons = await this.fetchPokemons(offset, limit);
+      const pokemonsDetails = await this.fetchDetailsForAllPokemons(pokemons);
 
       return pokemonsDetails;
     } catch (error) {
@@ -14,15 +10,30 @@ const pokeApi = {
     }
   },
 
-  getPokemonsDetail: async function(pokemons) {
-    const pokemonsDetails = await Promise.all(
-      pokemons.map(this.fetchPokemonDetail)
-    );
-    return pokemonsDetails;
+  fetchPokemons: async function(offset, limit) {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+    const req = await fetch(url);
+    const bodyJson = await req.json();
+    return bodyJson.results;
+  },
+
+  fetchDetailsForAllPokemons: async function(pokemons) {
+    return Promise.all(pokemons.map(pokemon => this.fetchPokemonDetail(pokemon)));
   },
 
   fetchPokemonDetail: async function(pokemon) {
     const pokemonReq = await fetch(pokemon.url);
-    return pokemonReq.json();
+    const response =  await pokemonReq.json();
+    return this.convertPokeApiDetailToPokemon(response);
+  },
+
+  convertPokeApiDetailToPokemon: function (pokeDetail) {
+    const pokemon = new Pokemon();
+    pokemon.id = pokeDetail.id;
+    pokemon.name = pokeDetail.name;
+    pokemon.types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+    pokemon.type = pokemon.types[0];
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+    return pokemon;
   }
 };
